e4f65f0a3f41685454c90f657347a8a3ba505de7
Norbert Reimer <norbertreimer2@gmail.com>
2020-09-09 14:21:12 +0000

    Init

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
 
 @app.route('/api/get_posts', methods=["POST"])
 def url():
@@ -11,4 +14,11 @@ def url():
     return jsonify({'result': url, 'msg': 'success'})
 def get_latest_posts():
   print('Getting')
+  resp = requests.get('https://www.instagram.com/dieweltentdecker/?__a=1')
+  data = resp.text
+  print(data)
   
+if __name__ == "__main__":
+    # the server will start
+    #app.run()
+    get_latest_posts()
